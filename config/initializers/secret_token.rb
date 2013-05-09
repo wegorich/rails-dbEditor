a1e6f21bd589be17c666316bb8bab7ef5fdb8b34
wego <eguurt@mail.ru>
2013-05-09 13:01:18 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Htdocs::Application.config.secret_key_base = '3afc9594e2ce9ec206579581b92b1891c64250d02a83b1f97c582ab6f50bea406c0dea003503c7c8f661fb9e767182b112036b6e0f2d8efa35159f7279f3eb79'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Htdocs::Application.routes.draw do
+  devise_for :users
+  resources :users
+  root to: 'home#index'
+end
