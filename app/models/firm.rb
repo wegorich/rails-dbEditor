5ae8c03ad938bd882ff80f2b3d1f2655bad0b23f
wego <eguurt@mail.ru>
2013-05-13 10:13:39 +0300

    fixed firms

--- app/models/firm.rb
+++ app/models/firm.rb
@@ -2,6 +2,8 @@ class Firm < ActiveRecord::Base
   has_many :specializables
   accepts_nested_attributes_for :specializables, :allow_destroy => true
 
+  has_many :specialties, through: :specializables
+
   UNRANSACKABLE_ATTRIBUTES = ["id"]
 
   def self.ransackable_attributes auth_object = nil
